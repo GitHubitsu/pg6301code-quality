8d20b1e97d1154aaf3b94b42da1d94e08020ec8e
tomsr <tom.skancke.rothe@gmail.com>
2023-07-20 17:13:28 +0200

    Add Simulate test for <NewMovieForm />

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import {ListMovies, NewMovieForm} from "../application";
+import {Simulate} from "react-dom/test-utils";
 
 describe("movies application", () => {
 
@@ -17,5 +18,20 @@ describe("movies application", () => {
         const element = document.createElement("div");
         ReactDOM.render(<NewMovieForm/>, element);
         expect(element.innerHTML).toMatchSnapshot();
+    });
+
+    it("submit new movie", () => {
+        const onAddMovie = jest.fn();
+
+        const element = document.createElement("div");
+        ReactDOM.render(<NewMovieForm onAddMovie={onAddMovie} />, element);
+
+        Simulate.change(element.querySelector("[data-testid=title]"), {target: {value: "Movie 1"}});
+        Simulate.change(element.querySelector("[data-testid=year]"), {target: {value: "2022"}});
+        Simulate.submit(element.querySelector("form"));
+
+        expect(onAddMovie).toBeCalledWith({
+            title: "Movie 1", year: "2022", plot: ""
+        })
     })
-});
+})
--- application.jsx
+++ application.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
+import {useState} from "react";
 
 export function ListMovies() {
-    return <div>
-        <h1>List movies</h1>
+    return <div><h1>List movies</h1>
         <ul>
             <li>Movie 1</li>
             <li>Movie 2</li>
@@ -10,18 +10,26 @@ export function ListMovies() {
     </div>;
 }
 
-export function NewMovieForm() {
-    return <form>
+export function NewMovieForm({onAddMovie}) {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit() {
+        onAddMovie({title, year, plot});
+    }
+
+    return <form onSubmit={handleSubmit}>
         <h1>Add new movie</h1>
         <div>
-            Title: <input/>
+            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
         <div>
-            Year: <input/>
+            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)} />
         </div>
         <div>
             Plot:
-            <div><textarea/></div>
+            <div><textarea data-testid="plot" /></div>
         </div>
     </form>;
 }
